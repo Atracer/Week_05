92735724f1128cb8a6785db125083117cff42534
peteryu <jiming2@illinois.edu>
2018-10-10 18:43:13 -0500

    a

--- src/App.js
+++ src/App.js
@@ -11,14 +11,35 @@ class App extends Component {
       "Add tea"
     ]
   };
-
+handleChange = event => {
+  this.setState({newTodo: event.target.value});
+};
+handleReplace = event => {
+  this.setState({
+    todos: [...this.state.todos]
+  })
+}
+handleSubmit = event => {
+  event.preventDefault();
+};
   render() {
     return (
       <div>
         <h1> How to make boba at home</h1>
         {this.state.todos.map(todo => (
-          <li key={todo}> {todo}</li>
+          <li key={todo}> {todo}
+            <button value = {todo} onClick = {this.handleChange}> X </button>
+          </li>
         ))}
+        <form onSumbit = {this.handleSubmit}>
+          <input
+          type = "text"
+          value = {this.state.value}
+          onChange = {this.handleChange}
+          />
+        <button onClick = {this.handleReplace}> + </button>
+        <input type="submit" value="Send Request"/>
+        </form>
       </div>
     );
   }
